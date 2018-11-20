a8239137596e14800f7321e029462ca65bfb6b9f
HABITATSOFT\elorente <elorente@pisos.com>
2018-11-20 17:05:18 +0100

    First state example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-class Title extends Component {
+class Contador extends Component {
+  constructor() {
+    super();
+    this.state = { contador: 1 };
+    setInterval(() => {
+      this.setState({ contador: this.state.contador + 1 });
+    }, 1000);
+  }
+
   render() {
-    return <h1>{this.props.text}</h1>;
+    return <ContadorNumero numero={this.state.contador} />;
   }
 }
 
-Title.defaultProps = {
-  text: "Hola"
-};
+class ContadorNumero extends Component {
+  render() {
+    console.log("ContadorNumero render()");
+    return <span>{this.props.numero}</span>;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Title text="Adeu" />
+        <p>Primer componente con state</p>
+        <Contador />
       </div>
     );
   }
