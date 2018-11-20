a5ad18aa4ce7d4b5be2e46e73038b354c9ad63c4
HABITATSOFT\elorente <elorente@pisos.com>
2018-11-20 15:43:36 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// function Hello(props) {
+//   return <h2>Welcom to React with a prop: {props.title}</h2>;
+// }
+
+// const Hello = props => <h2>Hello with props: {props.title}</h2>;
+
+class Hello extends Component {
+  render() {
+    return <h2>{this.props.title}</h2>;
+  }
+}
+
+class Text extends Component {
+  render() {
+    const textoSegunBool = this.props.boolean ? "Cierto" : "Falso";
+    const mappedNumbers = this.props.arrayOfNumbers.map(n => n * 2);
+
+    return (
+      <div>
+        <p>{this.props.text}</p>
+        <p>{this.props.number}</p>
+        <p>{textoSegunBool}</p>
+        <p>{mappedNumbers.join(", ")}</p>
+        <p>{this.props.objectWithInfo.key}</p>
+        <p>{this.props.objectWithInfo.key2}</p>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -8,17 +38,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Bienvenidos hijos del rock and roll</p>
+
+          <Hello title="React rules!!" />
+
+          <Text
+            arrayOfNumbers={[2, 3, 4]}
+            boolean={true}
+            objectWithInfo={{ key: "value", key2: "other value" }}
+            number={10}
+            text="Text 2"
+          />
         </header>
       </div>
     );
