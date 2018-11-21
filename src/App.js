4b802bbab109224c9e84aa430b3095fc3c7b4c52
HABITATSOFT\elorente <elorente@pisos.com>
2018-11-21 14:57:08 +0100

    Events example

--- src/App.js
+++ src/App.js
@@ -2,16 +2,39 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    const numbers = [1, 1, 3, 4, 5, 6];
+  constructor() {
+    super();
+    this.state = {
+      mouseX: 0,
+      mouseY: 0
+    };
+    //this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
 
+  handleMouseMove = e => {
+    const { clientX, clientY } = e;
+    this.setState({ mouseX: clientX, mouseY: clientY });
+  };
+
+  handleClick(e) {
+    alert("Hi, there!");
+  }
+
+  render() {
     return (
       <div className="App">
-        <h4>Trabajando don listas</h4>
+        <h4>Trabajando con eventos</h4>
 
-        {numbers.map((n, index) => {
-          return <p key={index}>Soy el número {n}</p>;
-        })}
+        <button onClick={this.handleClick}>Hi there!</button>
+
+        <div
+          onMouseMove={this.handleMouseMove}
+          style={{ border: "1px solid #000", marginTop: 10, padding: 10 }}
+        >
+          <p>
+            {this.state.mouseX}, {this.state.mouseY}
+          </p>
+        </div>
       </div>
     );
   }
