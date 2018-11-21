dbcbccf5ecb55b3970dfab2c72dbd5943dab78ae
HABITATSOFT\elorente <elorente@pisos.com>
2018-11-21 16:24:04 +0100

    Children examples

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
 import "./App.css";
-import Forms from "./sections/forms";
+
+class Box extends Component {
+  render() {
+    return (
+      <div style={{ border: "1px solid #000", margin: 5, padding: 5 }}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+class Article extends Component {
+  render() {
+    return (
+      <section>
+        <h2>{this.props.title}</h2>
+        <p>
+          <em>Escrito por {this.props.author}</em>
+        </p>
+        <Box>{this.props.date}</Box>
+        <article>{this.props.children}</article>
+      </section>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Forms />
+        <h4>Children props</h4>
+
+        <Article
+          author="Pepe"
+          date={new Date().toLocaleDateString()}
+          title="Artículo sobre la prop children"
+        >
+          Esto es el contenido del children
+        </Article>
       </div>
     );
   }
